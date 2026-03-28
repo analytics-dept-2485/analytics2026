c9d2137b4ada1ef10f7ecddf3b3092b31391948d
Maximilian Coyle McDonald <maximiliancoylemcdonald@Maxs-MacBook-Air.local>
2026-03-28 11:03:40 -0700

    foul counter major minor

--- src/app/form-components/FoulCounter.js
+++ src/app/form-components/FoulCounter.js
@@ -3,91 +3,94 @@ import { useState } from 'react';
 import styles from './FoulCounter.module.css';
 import CommentBox from './CommentBox';
 
-export default function FoulCounter({ visibleName, internalName, pieceType, min, max }) {
-    min = min || -99999;
+export default function FoulCounter({ internalName, min, max }) {
+    min = min || 0;
     max = max || 99999;
 
-    const [value, setValue] = useState(0);
-    
-    function incrementFive() {
-        if (value + 5 <= max) {
-            setValue(value + 5);
-            addCommentBox();
-        }
+    const [majorValue, setMajorValue] = useState(0);
+    const [minorValue, setMinorValue] = useState(0);
+
+    function increment(setValue, value) {
+        if (value + 1 <= max) setValue(value + 1);
     }
 
-
-    function decrementFive() {
-        if (value - 5 >= min) {
-            setValue(value - 5);
-        }
+    function decrement(setValue, value) {
+        if (value - 1 >= min) setValue(value - 1);
     }
-
-    function addCommentBox() {
-        return (
-            <CommentBox
-            visibleName={"General Comments"}
-            internalName={"generalcomments"}
-        />
-         )
-    }
-
-    // const handleFoulChange = (e) => {
-    //     const clicked = e.target.clicked;
-    //     setDefense(clicked);
-    //     if (onDefenseChange) onFoulChange(e);
-    // };
 
     return (
         <div className={styles.FoulCounter}>
-            <label className={styles.label} htmlFor={internalName}>{visibleName}</label>
-            <div className={styles.alignBox}>
-            <div className={styles.Container}>
-                <div className={styles.foulCounterBox}>
-                <div className={styles.decrementBox}>
-                    <button type="button" className={styles.DecButton} onClick={decrementFive}><h1><strong>-5</strong></h1></button>
-                </div>
-                
-                <div className={styles.inputBox}>  
-                    <h1>Foul Count</h1>
-                    <div className={styles.inputBox2}>
-                        <hr className={styles.line}></hr>
-                        <input 
+            <div className={styles.titleBox}>
+                <h1 className={styles.title}>Foul Count</h1>
+                <hr className={styles.Line} />
+            </div>
+
+            <div className={styles.countersRow}>
+                {/* Major Counter */}
+                <div className={styles.counterGroup}>
+                    <p className={styles.counterLabel}>Major:</p>
+                    <div className={styles.counterRow}>
+                        <button
+                            type="button"
+                            className={styles.DecButton}
+                            onClick={() => decrement(setMajorValue, majorValue)}
+                        >
+                            <strong>-</strong>
+                        </button>
+                        <input
                             className={styles.input}
                             type="number"
-                            id={internalName}
-                            name={internalName}
-                            value={value}
+                            id={`${internalName}-major`}
+                            name={`${internalName}-major`}
+                            value={majorValue}
                             readOnly
                         />
+                        <button
+                            type="button"
+                            className={styles.IncButton}
+                            onClick={() => increment(setMajorValue, majorValue)}
+                        >
+                            <strong>+</strong>
+                        </button>
                     </div>
-                </div>
-                <div className={styles.incrementBox}>
-                    <button type="button" 
-                    className={styles.IncButton} 
-                    onClick={incrementFive} 
-                     ><h1><strong>+5</strong></h1></button>
-                </div>
-                </div>
-                
-                 <div className={styles.fCommentBox}>
-                    <CommentBox
-                        visibleName={"Foul Elaboration"}
-                        internalName={"foulcomments"}
-                />
-                </div>
                 </div>
 
-                {/* {fouled && (
-                    <div className={styles.commentContainer}>
-                        <CommentBox 
-                            visibleName={"Foul Elaboration"}
-                            internalName={"foulcomments"}
+                {/* Minor Counter */}
+                <div className={styles.counterGroup}>
+                    <p className={styles.counterLabel}>Minor:</p>
+                    <div className={styles.counterRow}>
+                        <button
+                            type="button"
+                            className={styles.DecButton}
+                            onClick={() => decrement(setMinorValue, minorValue)}
+                        >
+                            <strong>-</strong>
+                        </button>
+                        <input
+                            className={styles.input}
+                            type="number"
+                            id={`${internalName}-minor`}
+                            name={`${internalName}-minor`}
+                            value={minorValue}
+                            readOnly
                         />
+                        <button
+                            type="button"
+                            className={styles.IncButton}
+                            onClick={() => increment(setMinorValue, minorValue)}
+                        >
+                            <strong>+</strong>
+                        </button>
                     </div>
-                )} */}
+                </div>
             </div>
-            <br/>
+
+            <div className={styles.fCommentBox}>
+                <CommentBox
+                    visibleName={"Foul Elaboration"}
+                    internalName={"foulcomments"}
+                />
+            </div>
         </div>
-    )
+    );
 }
